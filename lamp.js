998730c3e775f371e0b36ec4788b142107d86ec1
Cosmin <cos@192-168-0-145.rdsnet.ro>
2022-11-03 16:49:07 +0200

    Added more code.

--- lamp.js
+++ lamp.js
@@ -1,6 +1,9 @@
+
+
 const LAMP = {
     selectors: {
         // SELECTORS - Used to select content on the webpage.
+        title: document.querySelector('title'),
         meta: document.querySelectorAll('meta'),
         paragraphs: document.querySelectorAll('p'),
         h1: document.querySelectorAll('h1'),
@@ -11,17 +14,56 @@ const LAMP = {
     },
     // CONTENT - Used to store all the webpage as text.
     content: {
+        h2: {
+    
+        }
 
     },
+    words: {
+
+    },
+
     // START - Used to store all the functions
-    start: {
-        // Function - Used to grab webpage text and store them as objects.
-        update: () => {
+    run: {
+        main: () => {
+            LAMP.run.title();
+            LAMP.run.description();
+            LAMP.run.h2();
+        },
+
+        // 01 - Store Page Title
+
+        title: () => {
+            LAMP.content.title = {
+                text: LAMP.selectors.title.innerText,
+                chars: LAMP.selectors.title.innerText.length,
+                words: LAMP.selectors.title.innerText.split(' ').length
+
+            }
+        },
+        
+        // 02 - Store Meta Description
+
+        description: () => {
             LAMP.selectors.meta.forEach((element, index) => {
                 if (LAMP.selectors.meta[index].attributes[0].value === 'description') {
-                    LAMP.content.description = LAMP.selectors.meta[index].attributes[1].value;
+                    LAMP.content.description = element.attributes[1].value;
                 }
             })
+        },
+
+        // 03 - Store Headings 
+
+        h2: () => {
+            LAMP.selectors.h2.forEach((element, index) => {
+                 LAMP.content.h2[index] = {
+                    text: element.innerText,
+                    chars: element.innerText.length,
+                    words: element.innerText.split(' ').length
+                 }
+                 
+            })
         }
+        
     }
 }
