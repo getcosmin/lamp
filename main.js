05fac3dace54ea643331919f42b53335954d685f
Cosmin <cos@192-168-0-145.rdsnet.ro>
2022-11-12 22:24:37 +0200

    Update.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 try{
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    
     if(changeInfo.status == 'complete') {
         chrome.scripting.executeScript({
             files: ['lamp.js'],
             target: {tabId: tab.id}
         })
+
         chrome.scripting.insertCSS({
             files: ['lamp.css'],
             target: {tabId: tab.id}
